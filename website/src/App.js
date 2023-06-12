0699f93eace4616aecf3e468a771a7b10d90894e
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-06-12 11:20:54 -0500

    Started Progress

--- website/src/App.js
+++ website/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+//import react from 'react';
 import './App.css';
+import wtmw from './assets/wtmw.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="bg-black h-96 w-full bg-cover bg-center p-24 z-0">
+        <image src={wtmw} alt=""/>
+      </div>
     </div>
   );
 }
