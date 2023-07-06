b7233bee661e350a1f05cecd59964eb34950f0e0
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-07-06 09:22:41 -0500

    Update NavBar.js

--- website/src/assets/components/NavBar.js
+++ website/src/assets/components/NavBar.js
@@ -5,7 +5,7 @@ import { ArrowRightIcon } from "@heroicons/react/solid";
 export default function NavBar() {
     return (
         <header className="bg-violet-950 md:sticky top-0 z-10">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <div className="container flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a className="title-font font-medium text-white mb-4 md:mb-0">
                     <a href="#about" className="ml-3 text-xl">
                         Jerry Han
